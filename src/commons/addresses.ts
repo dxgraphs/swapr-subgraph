31e3d97de0937250b33e4f8a6e9cd5c08a92a6bb
Federico Luzzi <fedeluzzi00@gmail.com>
2021-02-22 12:27:22 +0100

    chore: new rinkeby staking rewards distribution factory address

--- src/commons/addresses.ts
+++ src/commons/addresses.ts
@@ -11,7 +11,7 @@ export function getFactoryAddress(): string {
 }
 
 const MAINNET_STAKING_REWARDS_FACTORY_ADDRESS = ADDRESS_ZERO
-const RINKEBY_STAKING_REWARDS_FACTORY_ADDRESS = '0x021Eedd456F872c952B1E58670aaD7DF4C5693f2'
+const RINKEBY_STAKING_REWARDS_FACTORY_ADDRESS = '0x2b2cbfC3F2D26789F224a65a5A0Fc2854EB8f0A1'
 
 export function getStakingRewardsFactoryAddress(): string {
   return dataSource.network() == 'rinkeby'
@@ -43,10 +43,10 @@ let MAINNET_WHITELIST: string[] = [
   '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
   '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
   '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
-  '0x514910771af9ca656af840dff83e8264ecf986ca', //LINK
-  '0x960b236a07cf122663c4303350609a66a7b288c0', //ANTyar
-  '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', //SNX
-  '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', //YFI
+  '0x514910771af9ca656af840dff83e8264ecf986ca', // LINK
+  '0x960b236a07cf122663c4303350609a66a7b288c0', // ANTyar
+  '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', // SNX
+  '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', // YFI
   '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', // yCurv
   '0xd533a949740bb3306d119cc777fa900ba034cd52' // CRV
 ]
