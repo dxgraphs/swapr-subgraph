7378e2a6c62fa78f5d47ac061ff8371547fdd086
Federico Luzzi <fedeluzzi00@gmail.com>
2021-03-29 21:40:25 +0200

    feat: add weth to liquidity tracking token addresses on xdai

--- src/commons/addresses.ts
+++ src/commons/addresses.ts
@@ -70,7 +70,8 @@ export function getLiquidityTrackingTokenAddresses(): string[] {
   if (network == 'xdai') {
     return [
       '0xe91d153e0b41518a2ce8dd3d7944fa863463a97d', // WXDAI
-      '0xb90d6bec20993be5d72a5ab353343f7a0281f158' // DXD
+      '0xb90d6bec20993be5d72a5ab353343f7a0281f158', // DXD
+      '0x6a023ccd1ff6f2045c3309768ead9e68f978f6e1' // WETH
     ]
   }
   log.warning('no liquidity tracking token address for unsupported network {}', [network])
