d8ca81db2faf6cdcc172650fae487563d1379520
Federico Luzzi <fedeluzzi00@gmail.com>
2021-09-14 22:41:16 +0200

    feat: handle mkr token name and symbol on arbitrum one

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prefer-const */
-import { log, BigInt, BigDecimal, Address, ethereum } from '@graphprotocol/graph-ts'
+import { log, BigInt, BigDecimal, Address, ethereum, dataSource } from '@graphprotocol/graph-ts'
 import { ERC20 } from '../types/Factory/ERC20'
 import { ERC20SymbolBytes } from '../types/Factory/ERC20SymbolBytes'
 import { ERC20NameBytes } from '../types/Factory/ERC20NameBytes'
@@ -64,11 +64,13 @@ export function isNullEthValue(value: string): boolean {
 
 export function fetchTokenSymbol(tokenAddress: Address): string {
   // hard coded overrides
-  if (tokenAddress.toHexString() == '0xe0b7927c4af23765cb51314a0e0521a9645f0e2a') {
-    return 'DGD'
+  let network = dataSource.network() as string
+  if (network == 'mainnet') {
+    if (tokenAddress.toHexString() == '0xe0b7927c4af23765cb51314a0e0521a9645f0e2a') return 'DGD'
+    if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') return 'AAVE'
   }
-  if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') {
-    return 'AAVE'
+  if (network == 'arbitrum-one') {
+    if (tokenAddress.toHexString() == '0x2e9a6df78e42a30712c10a9dc4b1c8656f8f2879') return 'MKR'
   }
 
   let contract = ERC20.bind(tokenAddress)
@@ -94,11 +96,13 @@ export function fetchTokenSymbol(tokenAddress: Address): string {
 
 export function fetchTokenName(tokenAddress: Address): string {
   // hard coded overrides
-  if (tokenAddress.toHexString() == '0xe0b7927c4af23765cb51314a0e0521a9645f0e2a') {
-    return 'DGD'
+  let network = dataSource.network() as string
+  if (network == 'mainnet') {
+    if (tokenAddress.toHexString() == '0xe0b7927c4af23765cb51314a0e0521a9645f0e2a') return 'DGD'
+    if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') return 'Aave token'
   }
-  if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') {
-    return 'Aave Token'
+  if (network == 'arbitrum-one') {
+    if (tokenAddress.toHexString() == '0x2e9a6df78e42a30712c10a9dc4b1c8656f8f2879') return 'Maker'
   }
 
   let contract = ERC20.bind(tokenAddress)
