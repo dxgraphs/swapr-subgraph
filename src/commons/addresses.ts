a88e3bdd47afa1b9798cc267228a221a6314ff98
Adam Azad <adam@adamazad.com>
2021-12-02 13:19:34 +0100

    fix: SWPR token address list

--- src/commons/addresses.ts
+++ src/commons/addresses.ts
@@ -145,14 +145,16 @@ export function getUsdtNativeCurrencyWrapperPair(): string {
 export function getSwaprTokenAddress(): string {
   let network = dataSource.network() as string
   // Production
-  if (network == 'mainnet') return '0x6cAcDB97e3fC8136805a9E7c342d866ab77D0957'
-  if (network == 'arbitrum-one') return '0xdE903E2712288A1dA82942DDdF2c20529565aC30'
+  if (network == 'mainnet') return '0x6cacdb97e3fc8136805a9e7c342d866ab77d0957'
+  if (network == 'arbitrum-one') return '0xde903e2712288a1da82942dddf2c20529565ac30'
   if (network == 'xdai') return '0x532801ED6f82FFfD2DAB70A19fC2d7B2772C4f4b'
   /**
    * @todo get the correct addreses
    */
-  if (network == 'arbitrum-rinkeby') return '0xdE903E2712288A1dA82942DDdF2c20529565aC30'
-  if (network == 'rinkeby') return '0xdE903E2712288A1dA82942DDdF2c20529565aC30'
+  if (network == 'arbitrum-rinkeby') return '0x99583f330814E04de96C0288FBF82B5E35A009dc'
+  if (network == 'rinkeby') return '0xa271ccbc126a41f04bae8fdbdbcefcf10bf59a48'
+  log.warning('no Swapr address for unsupported network {}', [network])
+  return ADDRESS_ZERO
 }
 
 /**
@@ -162,5 +164,5 @@ export function getSwaprTokenAddress(): string {
 export function isSwaprToken(address: Address): boolean {
   // let network = dataSource.network() as string;
   // for now, treat everything as true value
-  return address.toHexString() == getSwaprTokenAddress()
+  return address.toHexString().toLocaleLowerCase() == getSwaprTokenAddress().toLocaleLowerCase()
 }
